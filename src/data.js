16c08aab5dc60f7e278d4a6b62b095f9e5a204bc
sabbir <sabbirirfan29@gmail.com>
2024-03-27 06:07:53 +0600

    data update

--- src/data.js
+++ src/data.js
@@ -1,12 +1,12 @@
 export const Data = [
   {
     id: 11,
-    title: "Willy the Silly Panda",
+    title: "T-thocky KeyBoard",
     desc: "books",
     price: 120,
-    img: "https://cdn.stocksnap.io/img-thumbs/960w/chalet-wood_7PBFL1ERJT.jpg",
+    img: "https://vibegaming.com.bd/wp-content/uploads/2023/12/70a8e4d8eafd821987b3945050d5b48f.webp",
     amount:1,
-    catagory: "laptop"
+    catagory: "keyboard"
   },
   {
     id: 12,
@@ -20,28 +20,72 @@ export const Data = [
   {
     id: 13,
     title: "AulaF75",
-    desc: "books",
+    desc: "% pin Hotswappable chippest thocky Keyboard on the market",
     price: 120,
-    img: "https://m.media-amazon.com/images/I/81jv44QdNwL._AC_UY327_FMwebp_QL65_.jpg",
+    img: "https://vibegaming.com.bd/wp-content/uploads/2023/12/63e040d04fd3be341f4ce1d2c095f2e2.webp",
+    amount:1,
+    catagory: "keyboard"
+  },
+  {
+    id: 14,
+    title: "Mir zifriend",
+    desc: "books",
+    price: 157,
+    img: "https://vibegaming.com.bd/wp-content/uploads/2024/02/Untitled-design-27.png",
+    amount:1,
+    catagory: "keyboard"
+  }, {
+    id: 21,
+    title: "ATTACK SHARK X3 Lightweight Wireless Gaming Mouse Tri-Mode    ",
+    desc: `ATTACK SHARK X3 Lightweight Wireless Gaming Mouse, the ultimate choice for gamers. With versatile Tri-Mode connectivity (2.4G/USB-C Wired/Bluetooth), experience seamless gaming on PC, Laptop, Win, or Mac systems. 
+    Boasting an impressive 26K DPI and powered by the PAW3395 Optical Sensor and Kailh GM8.0 Switch, this mouse delivers unparalleled precision and responsiveness.
+    `,
+    price: 450,
+    img: "https://vibegaming.com.bd/wp-content/uploads/2023/10/61WZnwT6lL._AC_SX466_.webp",
     amount:1,
     catagory: "mouse"
   },
   {
-    id: 14,
-    title: "Aula F75",
-    desc: "books",
-    price: 157,
-    img: "https://m.media-amazon.com/images/I/81jv44QdNwL._AC_UY327_FMwebp_QL65_.jpg",
+    id: 22,
+    title: "GL Model D Wireless Gaming Mouse    ",
+    desc: `ATTACK SHARK X3 Lightweight Wireless Gaming Mouse, the ultimate choice for gamers. With versatile Tri-Mode connectivity (2.4G/USB-C Wired/Bluetooth), experience seamless gaming on PC, Laptop, Win, or Mac systems. 
+    Boasting an impressive 26K DPI and powered by the PAW3395 Optical Sensor and Kailh GM8.0 Switch, this mouse delivers unparalleled precision and responsiveness.
+    `,
+    price: 799,
+    img: "https://vibegaming.com.bd/wp-content/uploads/2022/05/D-wireless-Black.png",
     amount:1,
-    catagory: "monitor"
-  },{
-    id: 15,
-    title: "Willy the Silly Panda",
-    desc: "books",
-    price: 120,
-    img: "https://m.media-amazon.com/images/I/71fPuKH1r5L._AC_UY218_.jpg",
+    catagory: "mouse"
+  },
+  {
+    id: 17,
+    title: "GL Model O Wireless Gaming Mouse",
+    desc: `makes you feel every moment of your workflow with 
+             even more precision, tactility, and performance, thanks to Quiet Clicks and an 8,000 DPI track-on-glass 4 mm minimum glass thickness.`,
+    price: 555,
+    img: "https://vibegaming.com.bd/wp-content/uploads/2022/10/web-1.png",
     amount:1,
-    catagory: "laptop"
+    catagory: "mouse"
+  },
+  {
+    id: 18,
+    title: "Logitech MX Master 3S Wireless",
+    desc: `Glorious Model O Gaming Mouse | Wireless is impossibly lightweight, ambidextrous gaming mouse with lag-free connectivity and lightning-fast
+     performance with our BAMF wireless sensor. Vibe Gaming is the exclusive distributor of Glorious products in Bangladesh.
+    `,
+    price: 350 ,
+    img: "https://vibegaming.com.bd/wp-content/uploads/2022/05/1_white_matte_527055da-9b41-48ed-b1de-afd49b3fb5f4_1000x-4.webp",
+    amount:1,
+    catagory: "mouse"
+  },
+  {
+    id: 19,
+    title: "Fantech Venom II WGC2 Space Edition Wireless Gaming Mouse",
+    desc: `Fantech Venom II WGC2 Space Edition Wireless Gaming Mouse has connectivity: wireless, sensor type: optical, sensor: PixArt PAW3212 and adjustable LOD: no. Get free shipping on orders over 2500 BDT from Vibe Gaming
+    `,
+    price: 175,
+    img: "https://vibegaming.com.bd/wp-content/uploads/2023/12/WGC2_white_icon_758455p.png.png",
+    amount:1,
+    catagory: "mouse"
   },
   {
     id: 16,
@@ -143,6 +187,162 @@ export const Data = [
       amount:1,
       catagory: "book"
     },
+    // Monitors
+  {
+    id: 31,
+    title: "Samsung 32-inch UJ59 4K Gaming Monitor",
+    desc: "Ultra HD Monitor",
+    price: 379,
+    img: "https://images.unsplash.com/photo-1626827454475-fa0ee4f8540b",
+    amount: 1,
+    catagory: "monitor"
+  },
+  {
+    id: 32,
+    title: "Dell Ultrasharp U2718Q 27-Inch 4K Monitor",
+    desc: "Ultra HD Monitor",
+    price: 799,
+    img: "https://images.unsplash.com/photo-1585516510372-8a3b6a370c3f",
+    amount: 1,
+    catagory: "monitor"
+  },
+  {
+    id: 33,
+    title: "LG 27GN950-B 27 Inch UHD Nano IPS Display Ultragear Gaming Monitor",
+    desc: "Gaming Monitor",
+    price: 1296,
+    img: "https://images.unsplash.com/photo-1618957648878-b0ef3a178ac5",
+    amount: 1,
+    catagory: "monitor"
+  },
+  {
+    id: 34,
+    title: "Acer Predator XB273K Gpbmiipprzx 27-Inch UHD Gaming Monitor",
+    desc: "Gaming Monitor",
+    price: 699,
+    img: "https://images.unsplash.com/photo-1621089468691-6d9f5e83d5e7",
+    amount: 1,
+    catagory: "monitor"
+  },
+  {
+    id: 35,
+    title: "ASUS ROG Swift PG279Q 27-Inch Gaming Monitor",
+    desc: "Gaming Monitor",
+    price: 699,
+    img: "https://images.unsplash.com/photo-1601287032947-fb84d8658a8d",
+    amount: 1,
+    catagory: "monitor"
+  },
+  {
+    id: 36,
+    title: "BenQ EW3280U 32 inch 4K Monitor",
+    desc: "Entertainment Monitor",
+    price: 799,
+    img: "https://images.unsplash.com/photo-1612292794018-cce90ee7b9f6",
+    amount: 1,
+    catagory: "monitor"
+  },
+  {
+    id: 37,
+    title: "ViewSonic VX2458-C-MHD 24 Inch 1080p Curved Monitor",
+    desc: "Curved Gaming Monitor",
+    price: 169,
+    img: "https://images.unsplash.com/photo-1623690118602-bf1e7a47d43d",
+    amount: 1,
+    catagory: "monitor"
+  },
+  {
+    id: 38,
+    title: "HP VH240a 23.8 Inch Monitor",
+    desc: "Full HD Monitor",
+    price: 124,
+    img: "https://images.unsplash.com/photo-1561277523-36e73c51a37a",
+    amount: 1,
+    catagory: "monitor"
+  },
+  {
+    id: 39,
+    title: "Sceptre E248W-19203R 24 Inch Monitor",
+    desc: "Ultra Thin Monitor",
+    price: 159,
+    img: "https://images.unsplash.com/photo-1561277523-36e73c51a37a",
+    amount: 1,
+    catagory: "monitor"
+  },
+  {
+    id: 40,
+    title: "AOC CQ27G2 27 Super Curved Frameless Gaming Monitor",
+    desc: "Curved Gaming Monitor",
+    price: 409,
+    img: "https://images.unsplash.com/photo-1568003330947-e99446c78d84",
+    amount: 1,
+    catagory: "monitor"
+  },
+   // Laptops
+   {
+    id: 41,
+    title: "Apple MacBook Pro 16-Inch",
+    desc: "16-Inch Laptop",
+    price: 2399,
+    img: "https://images.unsplash.com/photo-1579546929516-22c63febff3c",
+    amount: 1,
+    catagory: "laptop"
+  },
+  {
+    id: 42,
+    title: "Dell XPS 15",
+    desc: "15-Inch Laptop",
+    price: 2099,
+    img: "https://images.unsplash.com/photo-1610836483809-fd6c88e720d2",
+    amount: 1,
+    catagory: "laptop"
+  },
+  {
+    id: 43,
+    title: "HP Spectre x360",
+    desc: "Convertible Laptop",
+    price: 1299,
+    img: "https://images.unsplash.com/photo-1616734793099-c70b8f2ae7d2",
+    amount: 1,
+    catagory: "laptop"
+  },
+  {
+    id: 44,
+    title: "Asus ROG Zephyrus G14",
+    desc: "Gaming Laptop",
+    price: 1499,
+    img: "https://images.unsplash.com/photo-1572212801362-34ff08f6a9b0",
+    amount: 1,
+    catagory: "laptop"
+  },
+  {
+    id: 45,
+    title: "Lenovo ThinkPad X1 Carbon",
+    desc: "Business Laptop",
+    price: 1699,
+    img: "https://images.unsplash.com/photo-1622828340024-06e2dd1a5fe0",
+    amount: 1,
+    catagory: "laptop"
+  },
+  {
+    id: 46,
+    title: "Microsoft Surface Laptop 4",
+    desc: "Touchscreen Laptop",
+    price: 1299,
+    img: "https://images.unsplash.com/photo-1619181111666-55a7a05a8b6f",
+    amount: 1,
+    catagory: "laptop"
+  },
+  {
+    id: 47,
+    title: "Razer Blade 15",
+    desc: "Gaming Laptop",
+    price: 1699,
+    img: "https://images.unsplash.com/photo-1589481262084-fd2a5df1b2ea",
+    amount: 1,
+    catagory: "laptop"
+  },
+
     
 ];
 
